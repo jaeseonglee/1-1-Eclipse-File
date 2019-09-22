52c73656ead8e2d9cf44dd0a7c98d17c743ac9e7
jaeseonglee <50550186+jaeseonglee@users.noreply.github.com>
2019-09-22 22:59:42 +0900

    Add files via upload

---
+++ One.java
@@ -0,0 +1,20 @@
+
+public class One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int age = 128;
+         long salary = 2500000000l;
+         double pi = 3.14; 		 
+         int grade = 2;
+         char credit = 'A';
+        		 
+         System.out.println("age = " + age);
+         System.out.println("salary = " +  salary);
+         System.out.println("pi = " + pi);   
+	     System.out.println("grade = " + grade);
+	     System.out.println("credit = " + credit);
+	     
+	}
+
+}
